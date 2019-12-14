3d5d772f5b2ef1c4b30a4d30737dc33b8f6fcf4c
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-12-14 10:42:28 +0800

    Update test_simple.py

--- test_simple.py
+++ test_simple.py
@@ -118,7 +118,7 @@ def test_simple(args):
             # Load image and preprocess
             input_image = pil.open(image_path).convert('RGB')
             if args.task_type == 'pku autonomous driving':
-                input_image = input_image[:, input_image.size[1]//2:]
+                input_image = input_image.crop([0, input_image.size[1]//2, input_image.size[0], input_image.size[1]])
             original_width, original_height = input_image.size
             input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
             input_image = transforms.ToTensor()(input_image).unsqueeze(0)
